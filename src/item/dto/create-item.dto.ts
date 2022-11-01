4958f253ee7791c302eb02f6dd7a4a00c6d7191e
Robson Fernando <robson.2501@hotmail.com>
2022-11-01 14:09:31 -0300

    feat: create item dto

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,1 +1,19 @@
-export class CreateItemDto {}
+import { IsNotEmpty, IsString} from 'class-validator';
+
+export class CreateItemDto {
+ @IsString()
+ @IsNotEmpty()
+ name: string;
+
+ @IsNotEmpty()
+ @IsString()
+ model: string;
+
+ @IsNotEmpty()
+ @IsString()
+ licensePlate: string;
+
+ @IsNotEmpty()
+ @IsString()
+ photo: string;
+}
