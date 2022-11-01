f490769acd80a526c94ac771af5db36d42eaa445
Robson Fernando <robson.2501@hotmail.com>
2022-11-01 15:15:35 -0300

    feat: updated swagger informations

--- src/carro/dto/create-carro.dto.ts
+++ src/carro/dto/create-carro.dto.ts
@@ -1,19 +1,27 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCarroDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'Monza' })
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: '2.0' })
   model: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'ABC-1952' })
   licensePlate: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example:
+      'https://png.pngtree.com/png-clipart/20190520/original/pngtree-cute-car-cartoon-vector-illustration-png-image_3755506.jpg',
+  })
   photo: string;
 }
