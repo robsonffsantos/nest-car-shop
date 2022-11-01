92740c4f2d3f1592cff24052dafc7b10d1839b82
Robson Fernando <robson.2501@hotmail.com>
2022-11-01 11:59:08 -0300

    config: docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [ItemModule],
+  imports: [
+    ItemModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3099,
+      username: 'carShop',
+      password: 'carShop',
+      database: 'carShop',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
