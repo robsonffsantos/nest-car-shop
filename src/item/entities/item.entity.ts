f1e7ce3907fe363a171c70d13eca6bc891ce7554
Robson Fernando <robson.2501@hotmail.com>
2022-11-01 14:04:18 -0300

    feat: update entity

--- src/item/entities/item.entity.ts
+++ src/item/entities/item.entity.ts
@@ -1,1 +1,25 @@
-export class Item {}
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Item extends BaseEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @CreateDateColumn()
+ createdAt: Date;
+
+ @UpdateDateColumn()
+ updatedAt: Date;
+
+ @Column({ name: 'name', type: 'varchar', length: 50 })
+ name: string;
+
+ @Column({ name: 'model', type: 'varchar', length: 50 })
+ model: string;
+
+ @Column({ name: 'licensePlate', type: 'varchar', length: 10 })
+ licensePlate: string;
+
+ @Column({ name: 'photo', type: 'varchar', length: 255 })
+ photo: string;
+}
